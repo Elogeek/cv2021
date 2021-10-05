f56a9ba7a1bb239a1ce569000853db120fc7fb58
elogeek <elodiechristin@gmail.com>
2021-10-05 14:26:14 +0200

    finish animation img form jQuery

--- asset/js/animationsJs/animate-img.js
+++ asset/js/animationsJs/animate-img.js
@@ -2,7 +2,7 @@
 
 $(document).ready( function() {
     $('#figureImg').hover(
-        function () {$(this).animate().css('rotateX(0deg)',1000).toggleClass('rotateImg').animate().css('rotateX(360deg)',1000)
+        function () {$(this).toggleClass('rotateImg')
         },
         function () {$(this).stop()}
     );
