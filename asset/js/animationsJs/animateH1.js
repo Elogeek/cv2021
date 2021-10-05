fd9678262ddc464380e6c1ac6292906171f62cb8
elogeek <elodiechristin@gmail.com>
2021-10-05 12:04:15 +0200

    start and finish animation h1 jQuery

--- asset/js/animationsJs/animateH1.js
+++ asset/js/animationsJs/animateH1.js
@@ -1,21 +1,20 @@
 // Animation h1
-let title = document.querySelector('h1');
+$(document).ready(function() {
+    $('h1').click(function() {
+        $(this).css(
+            {
+                'font-size': '4rem',
+                'color': 'black'
+            }
+        );
+    });
 
-title.addEventListener('click',startAnimationTitle);
-title.addEventListener('mouseout',stopAnimationTitle);
-
-/*
-Change color h1
- */
-function startAnimationTitle() {
-    title.style.color = 'black';
-    title.style.fontSize = '4rem';
-}
-
-/*
-Stop animation title (h1)
- */
-function stopAnimationTitle() {
-    title.style.color = 'white';
-    title.style.fontSize = '';
-}
+    $('h1').mouseout(function() {
+        $('h1').css(
+            {
+                'font-size': '',
+                 'color': 'white'
+            }
+        );
+    });
+});
--- asset/js/animationsJs/animateH2.js
+++ asset/js/animationsJs/animateH2.js
@@ -1,29 +1,18 @@
 // Animation h2
-let titleH2 = document.querySelector('h2');
+$(document).ready(function() {
 
-titleH2.addEventListener('mouseover',moveTitle);
-titleH2.addEventListener('mouseleave',stopAnimationTitle2);
+    $('h2').hover(function () {
 
-/*
- Move the title <h2>
- */
-function moveTitle() {
+            $(this).animate({transform: 'translateY(15px)'}).animate({transform: 'translateX(-300px)'})
+        },
+        function() {
+            $(this).stop(true);
+        })
+});
 
-    titleH2.animate([
-        // keyframes
-        { transform: 'translateY(15px)' },
-        { transform: 'translateX(-300px)' }
-    ], {
-        // timing options
-        duration: 1000,
-        iterations: 1
-    });
 
-}
 
-/*
-  Stop animation title <h2>
- */
-function  stopAnimationTitle2() {
-    titleH2.style.animationPlayState = 'paused';
-}
+
+
+
+
